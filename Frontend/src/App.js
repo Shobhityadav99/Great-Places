8699576d2a40df9efe71b2b09024fa1e2b866fc4
Shobhityadav99 <shobhit999000@gmail.com>
2021-09-07 11:38:29 +0530

    Adding first user

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route , Switch} from 'react-router-dom';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return <Router>
-    
+    <Switch>
+    <Route path="/">
+      <Users />
+    </Route>
+    <Route path="/new">
+      <NewPlace />
+    </Route>
+    </Switch>
   </Router>
 }
 
--- Frontend/src/user/pages/Users.js
+++ Frontend/src/user/pages/Users.js
@@ -1,5 +1,13 @@
+import React from "react";
+import UsersList from '../components/UsersList';
 const Users = () => {
-    return <h2> User Workss!!</h2>;
+    const USERS = [{
+        id: 'a1',
+        name: 'Shobhit',
+        image: 'shorturl.at/crGOY',
+        places: 3
+    }];
+    return <UsersList items = {USERS} />
 }
 
 export default Users;
