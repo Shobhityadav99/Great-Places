ab7c1600a66679b4496fc24b97e06f5dd9c55d20
Shobhityadav99 <shobhit999000@gmail.com>
2021-09-07 12:37:52 +0530

    Links added in drawer

--- Frontend/src/shared/components/Navigation/MainNavigation.js
+++ Frontend/src/shared/components/Navigation/MainNavigation.js
@@ -4,21 +4,30 @@ import MainHeader from "./MainHeader";
 
 import './MainNavigation.css';
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 
 const MainNavigation = props => {
-    return (<MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span />
-            <span />
-            <span />
-        </button>
-        <h1 className="main-navigation__title">
-            <Link to="/">Your Places</Link>
-        </h1>
-        <nav className="main-navigation__header-nav">
-            <NavLinks />
-        </nav>
-    </MainHeader>);
+    return (
+        <React.Fragment>
+            <SideDrawer>
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+            <MainHeader>
+                <button className="main-navigation__menu-btn">
+                    <span />
+                    <span />
+                    <span />
+                </button>
+                <h1 className="main-navigation__title">
+                    <Link to="/">Your Places</Link>
+                </h1>
+                <nav className="main-navigation__header-nav">
+                    <NavLinks />
+                </nav>
+            </MainHeader>
+        </React.Fragment>);
 }
 
 export default MainNavigation;
--- Frontend/src/shared/components/Navigation/SideDrawer.js
+++ Frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import ReactDom from "react-dom";
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-    
+    const content =  <aside className="side-drawer">{props.children}</aside>;
+
+    return ReactDOM.createPortal(content,document.getElementById('drawer-hook'))
 };
 
 export default SideDrawer;
