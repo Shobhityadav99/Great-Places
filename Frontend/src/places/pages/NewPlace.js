3d588c3432928e6560f44b3e10ab30984b944ebb
Shobhityadav99 <shobhit999000@gmail.com>
2021-09-09 11:36:05 +0530

    Input Component added

--- Frontend/src/places/pages/NewPlace.js
+++ Frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+
+import './NewPlace.css';
 
 const Users = () => {
-    return <h2> New Place Workss!!</h2>;
+    return <form className="place-form">
+        <Input type="text" element="input" label="Title" />
+    </form>
 }
 
 export default Users;
