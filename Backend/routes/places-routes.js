a9f3290281b0c5f733245a0d76330eb7393f17c9
Shobhityadav99 <shobhit999000@gmail.com>
2021-09-10 20:13:55 +0530

    Adding routes

---
+++ Backend/routes/places-routes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  if(!place){
+    return res.status(404).json({"message": "Could not find places for provided id."})
+  }
+  res.json({place}); // => { place } => { place: place }
+});
+
+router.get('/users/:uid',(req,res,next)=> {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+  if(!place){
+    return res.status(404).json({"message": "Could not find places for provided user id."})
+  }
+  res.json({place});
+})
+
+module.exports = router;
