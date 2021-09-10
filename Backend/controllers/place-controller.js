2a71dbcc2af8207cc6e97f9765c231797f1e2dec
Shobhityadav99 <shobhit999000@gmail.com>
2021-09-10 21:15:43 +0530

    Controllers in progress

---
+++ Backend/controllers/place-controller.js
@@ -0,0 +1,45 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th St, New York, NY 10001',
+        creator: 'u1'
+    }
+];
+
+const getplaceById = (req, res, next) => {
+    const placeId = req.params.pid; // { pid: 'p1' }
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId;
+    });
+    if (!place) {
+        throw new HttpError('Could not find places for provided id', 404);
+    }
+    res.json({ place }); // => { place } => { place: place }
+}
+
+const getplaceByuserId = (req, res, next) => {
+    const userId = req.params.uid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    });
+    if (!place) {
+        return next(new HttpError('Could not find places for provided user id', 404));
+    }
+    res.json({ place });
+}
+
+const createPlace = (req, res, next) => {
+
+}
+
+exports.getplaceById = getplaceById;
+exports.getplaceByuserId = getplaceByuserId;
+exports.createPlace = createPlace;
