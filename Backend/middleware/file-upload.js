63c05f8aea74de8c8bacaec00e8cb7a3de7370db
Shobhityadav99 <shobhit999000@gmail.com>
2021-12-14 19:14:33 +0530

    wiring up frontend with backend

--- Backend/middleware/file-upload.js
+++ Backend/middleware/file-upload.js
@@ -2,9 +2,9 @@ const multer = require('multer');
 const uuid = require('uuid/v1');
 
 const MIME_TYPE_MAP = {
-    'image/png': 'png',
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpeg',
+    'image/png': '.png',
+    'image/jpg': '.jpg',
+    'image/jpeg': '.jpeg',
 }
 
 const fileUpload = multer({
